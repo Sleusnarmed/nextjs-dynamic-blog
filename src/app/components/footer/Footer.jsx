086c06cf0118d5837f5b476e8762eb6aaa766513
José Salazar <jmsalazar.macg@hotmail.com>
2024-01-06 00:13:40 -0600

    footer responsive

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -9,8 +9,10 @@ const Footer = () => {
       <div className={styles.info}>
         <div className={styles.logo}>
           <div className={styles.logoImage}>
-            <Image src="/logo.png" alt="Sleus blog" width={50} height={50}/>
+            <Image src="/logo.png" alt="Sleus blog" width={50} height={50} />
+
           </div>
+
           <h1 className={styles.logoText}>Blog App</h1>
         </div>
         <p className={styles.desc}>
@@ -20,33 +22,33 @@ const Footer = () => {
           assumenda quo amet voluptatem.
         </p>
         <div className={styles.icons}>
-          <Image src="/facebook.png" alt="facebook" width={18} height={18}/>
-          <Image src="/instagram.png" alt="instagram" width={18} height={18}/>
-          <Image src="/twitter.png" alt="twitter" width={18} height={18}/>
-          <Image src="/linkedin.png" alt="linkedin" width={18} height={18}/>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+          <Image src="/twitter.png" alt="twitter" width={18} height={18} />
+          <Image src="/linkedin.png" alt="linkedin" width={18} height={18} />
         </div>
       </div>
       <div className={styles.links}>
         <div className={styles.list}>
           <span className={styles.listTitle}>Links</span>
-          <Link href='/'>Homepage</Link>
-          <Link href='/'>Blog</Link>
-          <Link href='/'>About</Link>
-          <Link href='/'>Contact</Link>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Tags</span>
-          <Link href='/'>Style</Link>
-          <Link href='/'>Fashion</Link>
-          <Link href='/'>Coding</Link>
-          <Link href='/'>Travel</Link>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Social</span>
-          <Link href='/'>Facebook</Link>
-          <Link href='/'>Instagram</Link>
-          <Link href='/'>Twitter</Link>
-          <Link href='/'>Linkedin</Link>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Twitter</Link>
+          <Link href="/">Linkedin</Link>
         </div>
       </div>
     </div>
