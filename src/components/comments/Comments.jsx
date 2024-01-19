fb5978788286cb0ad87262cfd24186566613d8ca
José Salazar <jmsalazar.macg@hotmail.com>
2024-01-18 21:17:36 -0600

    add swr/comments fetched data

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,9 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./comments.module.css";
 import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
 
-const Comments = () => {
-  const status = "authenticated";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
+  }
+
+  return data;
+};
+
+const Comments = ({ postSlug }) => {
+  const { status } = useSession();
+
+  const { data, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
+  );
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
@@ -16,27 +38,28 @@ const Comments = () => {
         <Link href="/login"> Login to write</Link>
       )}
       <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              width={50}
-              height={50}
-              className={styles.image}
-              alt=""
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Jose Salazar</span>
-              <span className={styles.date}>01.01.2024</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae
-            repudiandae omnis saepe quia nihil, excepturi explicabo, dolorum
-            fugiat recusandae ipsa sapiente earum rem quod autem deserunt
-            laboriosam dolorem consectetur ex!
-          </p>
-        </div>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image &&  <Image
+                      src={item.user.image}
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                      alt=""
+                    />}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt.slice(0,10)}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>
+                  {item.desc}
+                </p>
+              </div>
+            ))}
       </div>
     </div>
   );
